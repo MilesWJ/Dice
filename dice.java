37255328f4d78b50d43701b633d90df59ee98e7f
Miles James <milesw.james@gmail.com>
2021-02-21 09:06:55 -0800

    Update dice.java

--- dice.java
+++ dice.java
@@ -10,11 +10,10 @@ public class dice {
         System.out.print("\nEnter the maximum value: ");
         int max = Input.nextInt();
 
+        Input.close();
+
         int dice = (int) (Math.random() * max - min + 1) + min;
 
         System.out.println("\nDice roll: " + dice);
-
-        Input.close();
     }
-
 }
